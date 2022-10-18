e6099fa17d9c576da6aa1e76f11eb49796fcb5a4
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-10-18 09:01:11 +0200

    Completed first version of CMake tests for ARPACK

---
+++ build-and-test-cmake.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+branch="cmake"
+prefix=`pwd`/local
+#rm -rf build build-arpack local
+rm -rf  build-cmake-test
+if [ ! -d $prefix/lib ]; then
+	if [ ! -d arpack-ng ]; then
+		git clone https://github.com/juanjosegarciaripoll/arpack-ng.git
+		git checkout --track origin/$branch
+	fi
+	rm -rf build-arpack
+	mkdir build-arpack
+	cmake -S ./arpack-ng -B build-arpack -D CMAKE_INSTALL_PREFIX="$prefix" -D ICB=ON -D ICBEXM=ON -D MPI=ON
+	cmake --build build-arpack
+	cmake --install build-arpack
+fi
+#cat arpack-ng/TESTS/icb_arpack_c.c | sed 's,#include ",#include "arpack-ng/,g' > cmake-test/icb_arpack_c.c
+#cat arpack-ng/TESTS/icb_arpack_cpp.cpp | sed 's,#include ",#include "arpack-ng/,g' > cmake-test/icb_arpack_cpp.cpp
+cp arpack-ng/TESTS/icb_arpack_c.c cmake-test/icb_arpack_c.c
+cp arpack-ng/TESTS/icb_arpack_cpp.cpp cmake-test/icb_arpack_cpp.cpp
+rm -rf build-cmake-test
+cmake -S cmake-test -B build-cmake-test -G "Unix Makefiles" -D CMAKE_PREFIX_PATH="$prefix/lib/cmake/arpack-ng"
+cmake --build build-cmake-test
+if ./build-cmake-test/icb_arpack_c 2>&1 >build-cmake-test/icb_arpack_c.log; then
+	echo Succeeded icb_arpack_c
+else
+	echo Failed icb_arpack_c
+	exit -1
+fi
+if ./build-cmake-test/icb_arpack_cpp 2>&1 >build-cmake-test/icb_arpack_cpp.log; then
+	echo Succeeded icb_arpack_cpp
+else
+	echo Failed icb_arpack_cpp
+	exit -1
+fi
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 branch="cmake"
 prefix=`pwd`/local
-if [ ! -d arpack-ng ]; then
-	git clone https://github.com/juanjosegarciaripoll/arpack-ng.git
-	git checkout --track origin/$branch
+rm -rf build build-arpack build-cmake-test local
+if [ ! -d local/lib ]; then
+	if [ ! -d arpack-ng ]; then
+		git clone https://github.com/juanjosegarciaripoll/arpack-ng.git
+		git checkout --track origin/$branch
+	fi
+	rm -rf build
+	mkdir build
+	if [ ! -f arpack-ng/Makefile.in ]; then
+		(cd ./arpack-ng && ./bootstrap)
+	fi
+	cd build
+	../arpack-ng/configure --prefix=$prefix --enable-icb --enable-icb-exmm --enable-mpi
+	make -j4
+	make -j4 install
 fi
-rm -rf build
-mkdir build
-if [ ! -f arpack-ng/Makefile.in ]; then
-	(cd ./arpack-ng && ./bootstrap)
-fi
-cd build
-../arpack-ng/configure --prefix=$prefix --enable-icb --enable-icb-exmm --enable-mpi
-make -j4
-make -j4 install
+cd makedir-test
+make clean
+make
